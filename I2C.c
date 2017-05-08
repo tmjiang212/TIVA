becd3d1bb942ffaa1b829ddac2ff9e0e91d654b8
tmjiang212 <tmjiang8152@gmail.com>
2017-05-08 11:10:47 -0700

    Update I2C.c

--- I2C.c
+++ I2C.c
@@ -11,31 +11,28 @@
 
 #define SLAVE_ADDRESS 0x3C
 
-void    delay   (void)
+void delay(void)
 {
-  volatile uint32_t ui32Loop;   
+    volatile uint32_t ui32Loop;   
     for(ui32Loop = 0; ui32Loop < 200; ui32Loop++);
 }
 
 
 volatile uint32_t  result;
-    uint32_t    g_ui32SysClock;
+uint32_t    g_ui32SysClock;
 
 int main(void)
 {
+    //Set Clock
     g_ui32SysClock = SysCtlClockFreqSet((SYSCTL_OSC_MAIN | SYSCTL_XTAL_25MHZ | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_320), 40000000); 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
+
+    // Enable the GPIO 
     SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R3 | SYSCTL_RCGCGPIO_R9 | SYSCTL_RCGCGPIO_R1;
-    //
+
     // Do a dummy read to insert a few cycles after enabling the peripheral.
-    //
     result = SYSCTL_RCGCGPIO_R;
-    //
-    // Enable the GPIO pin for the LED (PD3).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
+
+    // Enable the GPIO pin for the LED (PD3).  Set the direction as output, and enable the GPIO pin for digital function.
     GPIO_PORTD_AHB_DIR_R = 0x8;
     GPIO_PORTD_AHB_DEN_R = 0x8;
 
@@ -51,46 +48,35 @@ int main(void)
     GPIOPinTypeI2C(GPIO_PORTB_BASE, (1 << 7));       // Configures SDA
     GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, (1 << 6));    // Configures SCL
 
+    //Set I2C Master Clock
     I2CMasterInitExpClk(I2C4_BASE, g_ui32SysClock, false);
 
     I2CSlaveEnable(I2C6_BASE);
     I2CSlaveInit(I2C6_BASE, SLAVE_ADDRESS);
     I2CMasterSlaveAddrSet(I2C4_BASE, SLAVE_ADDRESS, false);
-    //
+
     // Loop forever.
-    //
     while(1)
     {
-        //
+
         // Turn on the LED.
-        //
         GPIO_PORTD_AHB_DATA_R |= 0x8;
 
         I2CMasterDataPut(I2C4_BASE, 0x33);
         I2CMasterControl(I2C4_BASE, I2C_MASTER_CMD_SINGLE_SEND);
-        //
+      
         // Wait until the slave has received and acknowledged the data.
-        //
         while(!(I2CSlaveStatus(I2C6_BASE) & I2C_SLAVE_ACT_RREQ));           
-        //
         // Read the data from the slave.
-        //
         result = I2CSlaveDataGet(I2C6_BASE);
-        //
+
         // Wait until master module is done transferring.
-        //
         while(I2CMasterBusy(I2C4_BASE));
-        //
-        // Delay for a bit.
-        //
         delay   ();
-        //
+
         // Turn off the LED.
-        //
         GPIO_PORTD_AHB_DATA_R &= ~(0x8);
-        //
-        // Delay for a bit.
-        //
-        delay   ();
+
+        delay();
     }
 }
